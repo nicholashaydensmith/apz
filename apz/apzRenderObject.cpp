2f182a79b1376287592d416192854d2dbf59000a
Nicholas Hayden Smith <nihasmit@ucsc.edu>
2013-10-23 14:36:02 -0700

    Upadated Project Direction

--- apz/apzRenderObject.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdafx.h"
-#include "apzRenderObject.h"
-#include "apzUtilities.h"
-
-using namespace apz;
-
-void RenderObject::Render(MATRIX_RENDER_INFO_T &tMatrixInfo, Shader &tShaderInfo) {
-
-}
